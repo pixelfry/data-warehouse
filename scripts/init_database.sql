4f4085d2fb2ecd188c870efe57530aa250cdf18c
fries <jjlauesen@gmail.com>
2025-08-26 15:24:43 -0400

    added tabel script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -19,7 +19,7 @@ GO
 -- Drop and recreate the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-        ALTER DATABASE DataWarehouse SET SINGLUE_USER WITH ROLLBACK IMMEDIATE;
+        ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
         DROP DATABASE DataWarehouse;
 END;
 GO
